4dcacc1ed2979d02a568797147d03921785c195b
v14dt20 <91105780+v14dt20@users.noreply.github.com>
2021-12-17 17:11:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-bot_token="8ae066eed3178fdaf1a79e51b30ab52a53d83cf5d823d7c0aa3c3c23061d1ed79284d2b45311ee7f76efe"
+bot_token="eb1e898a02fcfac36062a9607fe70d6ba235fa9f56487e93de714975b6d9f83a83c52463356010fa7a21e"
 DB_URI = "postgres://ugraydkltohwyr:7969ef3d14490f3490b1f50fb40f06bd94b944b2b4680c5c750ac2dc98f40f62@ec2-18-202-156-92.eu-west-1.compute.amazonaws.com:5432/d6950pgb7oqric"
