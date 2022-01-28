bc47732d284d24e10e310e5f7d7751680037adab
v14dt20 <91105780+v14dt20@users.noreply.github.com>
2022-01-29 00:51:37 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 bot_token="eb1e898a02fcfac36062a9607fe70d6ba235fa9f56487e93de714975b6d9f83a83c52463356010fa7a21e"
-DB_URI = "postgres://ugraydkltohwyr:7969ef3d14490f3490b1f50fb40f06bd94b944b2b4680c5c750ac2dc98f40f62@ec2-18-202-156-92.eu-west-1.compute.amazonaws.com:5432/d6950pgb7oqric"
+DB_URI = "postgres://reydbmdypptovl:c67483f82bd4fb36f5f2c855247bf9774017b589daa276aa28ce22ad3a6a1f75@ec2-34-249-49-9.eu-west-1.compute.amazonaws.com:5432/dbuf22tv4oqct8"
